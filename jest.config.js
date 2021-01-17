a5c5e98a1eccfa5353709eda77c9e5dbdf0b9e81
bull0n <lucas.bulloni@protonmail.ch>
2021-01-17 21:40:07 +0100

    update config for code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue, ts}',
+    '!src/main.ts',
+    '!src/router/index.ts',
+    '!src/store/index.ts',
+  ],
   moduleFileExtensions: [
     'js',
     'ts',
